602fe9b30a0915dceb8539aaf3f0581bd3ad5e3e
Argadida <149749065+Argadida@users.noreply.github.com>
2024-03-25 10:10:23 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors : {
+        df1: "#252842",
+        df2: "#394284"
+      }
+    },
+  },
+  plugins: [],
+  darkMode: "class"
+}
+
